ba5ac267d221d6140b7a0df01ceeea58c1c639a2
Piotr Romańczak <piotr.romanczak@gmail.com>
2021-11-14 17:13:29 +0100

    GAM-16 added async call to get matched games

--- src/app/components/match-section/match-section.component.ts
+++ src/app/components/match-section/match-section.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GameCategoryListService } from 'src/app/services/game-category-list.service';
 import { GameCategory } from 'src/app/common/game/game-category';
+import { Game } from 'src/app/common/game/game';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-match-section',
@@ -14,9 +17,18 @@ export class MatchSectionComponent implements OnInit {
   selectedItems: GameCategory[] = [];
   dropdownSettingsSingle: IDropdownSettings = {};
   dropdownSettings: IDropdownSettings = {};
-  gameCategories: GameCategory[];
+  gameCategories: GameCategory[] = [];
+  emptyCategoryMessage: string = '';
+  categoriesToMatch: GameCategory[] = [];
+  matchedGames: Game[] = [];
+  gameToShow: Game = new Game;
+  gameIndexToShow: number = 0;
+  runAgainFlag: Boolean = true;
 
-  constructor(private gameListService: GameCategoryListService) { }
+  constructor(
+    private gameCategoryListService: GameCategoryListService,
+    private gameService: GameService,
+    private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.listGameCategories();
@@ -41,26 +53,87 @@ export class MatchSectionComponent implements OnInit {
   }
 
   listGameCategories() {
-    this.gameListService.getGameCategoryList().subscribe(
+    this.gameCategoryListService.getGameCategoryList().subscribe(
       data => {
-        this.gameCategories = data;
+        this.gameCategories = data; 
       }
     )
   }
 
-  onItemSelect(item: any) {
-    this.selectedItems.push(item);
-  }
-  onItemSelectSingle(item: any) {
-    this.selectedItemsSingle.push(item);
-  }
-  onDeSelectSingle() {
-    this.selectedItemsSingle = [];
+  async getGamesByUserMatch() {
+    this.gameService.getGameMatch(this.categoriesToMatch).then(
+      async data => {
+        this.matchedGames = await data;                
+        this.setGameToShow();
+      }
+    )
   }
 
-  onDeSelect(item: any) {
+  setGameToShow() {
+    this.gameToShow = this.matchedGames[this.gameIndexToShow];
+  }
+
+  setMatchingCategories() {
+    this.categoriesToMatch = [];
+    this.selectedItemsSingle.forEach((value) => {
+      this.categoriesToMatch.push(value);
+    });
+    this.selectedItems.forEach((value) => {
+      this.categoriesToMatch.push(value);
+    });
+  }
+
+  /**single multiselect dropdown handlers*/
+  handleSingleSelect(item: any) {
+    this.selectedItemsSingle = [];
+    this.selectedItemsSingle.push(item);
+    this.runAgainFlag = true;
+  }
+
+  handleSingleDeselect() {
+    this.selectedItemsSingle = [];
+    this.runAgainFlag = true;
+  }
+
+  /**multiple multiselect dropdown handlers*/
+  handleSelect(item: any) {
+    this.selectedItems.push(item);
+    this.runAgainFlag = true;
+  }
+
+  handleDeselect(item: any) {
     this.selectedItems.forEach((value, index) => {
       if (value.gameCategoryId == item.gameCategoryId) this.selectedItems.splice(index, 1);
     });
+    this.runAgainFlag = true;
+  }
+
+  handleDeselectAll() {
+    this.selectedItems = [];
+    this.runAgainFlag = true;
+  }
+
+  handleSelectAll() {
+    this.selectedItems = [];
+    this.selectedItems = this.gameCategories;
+    this.runAgainFlag = true;
+  }
+
+  /**gameMatch handlers */
+  handleUserMatch(content: any) {
+    if (this.selectedItemsSingle.length == 0) {
+      this.emptyCategoryMessage = 'Należy wybrać podstawowe kryterium wyszukiwania';
+    } else if (this.selectedItemsSingle.length > 0 && this.runAgainFlag == true) {
+      this.emptyCategoryMessage = '';
+      this.setMatchingCategories();
+      try {
+        this.getGamesByUserMatch();                
+        this.modalService.open(content, { size: 'lg' });           
+      } finally {
+        this.runAgainFlag = false;
+      }
+    } else {
+      this.modalService.open(content, { size: 'lg' });
+    }
   }
 }
