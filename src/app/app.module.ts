8b7f95e17207f9eb78c5bd7e9ecf992c2bd15424
Piotr Romańczak <piotr.romanczak@gmail.com>
2021-12-18 18:45:17 +0100

    GAM-23 modules replacement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,50 @@
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl';
 import { LOCALE_ID, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-import localePl from '@angular/common/locales/pl';
+
 registerLocaleData(localePl);
 
 import { AppComponent } from './app.component';
+import { RoutingModule } from './routing.module';
+import { SafePipe } from './safe.pipe';
+
+/** custom components */
 import { GameListComponent } from './components/game-list/game-list.component';
-import { GameService } from './services/game.service';
 import { HeaderComponent } from './components/header/header.component';
 import { MatchSectionComponent } from './components/match-section/match-section.component';
-import { GameCategoryListService } from './services/game-category-list.service';
 import { GameRecordPageComponent } from './components/game-record-page/game-record-page.component';
-import { RoutingModule } from './routing.module';
 import { HeaderAdBannerComponent } from './components/header-ad-banner/header-ad-banner.component';
-import { SafePipe } from './safe.pipe';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
-import { CarouselModule } from 'primeng/carousel';
-import { ButtonModule } from 'primeng/button';
-import { PanelModule } from 'primeng/panel';
-import { DataViewModule } from 'primeng/dataview';
-import {DropdownModule} from 'primeng/dropdown';
-import {MenubarModule} from 'primeng/menubar';
-import {InputTextModule} from 'primeng/inputtext';
-import {CardModule} from 'primeng/card';
-import {MultiSelectModule} from 'primeng/multiselect';
-import {TabViewModule} from 'primeng/tabview';
 import { GameMatchListComponent } from './components/game-match-list/game-match-list.component';
-import {RatingModule} from 'primeng/rating';
-import {DialogModule} from 'primeng/dialog';
-import { ChipModule } from 'primeng/chip';
-import { FormsModule } from '@angular/forms';
-import {KnobModule} from 'primeng/knob';
 import { SimilarGamesComponent } from './components/similar-games/similar-games.component';
 import { PopularGamesComponent } from './components/popular-games/popular-games.component';
 import { GenericDataViewComponent } from './components/generic-data-view/generic-data-view.component';
 import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
-import {ToastModule} from 'primeng/toast';
+
+/** services */
+import { GameService } from './services/game.service';
+import { GameCategoryListService } from './services/game-category-list.service';
+
+/** PrimeNG modules */
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
+import { DataViewModule } from 'primeng/dataview';
+import { DropdownModule } from 'primeng/dropdown';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { TabViewModule } from 'primeng/tabview';
+import { RatingModule } from 'primeng/rating';
+import { DialogModule } from 'primeng/dialog';
+import { ChipModule } from 'primeng/chip';
+import { FormsModule } from '@angular/forms';
+import { KnobModule } from 'primeng/knob';
+import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 
 @NgModule({
@@ -62,7 +68,6 @@ import { MessageService } from 'primeng/api';
     BrowserAnimationsModule,
     HttpClientModule,
     RoutingModule,
-    NgMultiSelectDropDownModule,
     CarouselModule,
     ButtonModule,
     DataViewModule,
